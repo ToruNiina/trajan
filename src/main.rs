abd165230534be75139d62fb601211f90850670b
ToruNiina <niina.toru.68u@gmail.com>
2018-12-24 01:20:57 +0900

    make XYZParticle generic

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,36 @@
 use std::io::BufRead; // for BufReader.lines
+use std::io::Error;
+use std::io::ErrorKind;
 use std::str::FromStr;
-#[macro_use]
-extern crate soa_derive;
+use nalgebra::base::Scalar;
+// #[macro_use]
+// extern crate soa_derive;
 
-#[derive(Debug, PartialEq, StructOfArray)]
-#[soa_derive = "Debug, PartialEq"]
-pub struct XYZParticle {
+// #[derive(Debug, PartialEq, StructOfArray)]
+// #[soa_derive = "Debug, PartialEq"]
+#[derive(Debug, PartialEq)]
+pub struct XYZParticle<T: Scalar+FromStr> {
     pub name  : std::string::String,
-    pub coord : nalgebra::Vector3<f64>,
+    pub coord : nalgebra::Vector3<T>,
 }
-// to implement the following, we need a number of of trait boundaries and
-// tons of boilerplates for error handlings.
-// pub struct XYZParticle<T: nalgebra::base::Scalar> {
-//     pub name  : std::string::String,
-//     pub coord : nalgebra::Vector3<T>,
-// }
 
-impl FromStr for XYZParticle {
-    type Err = std::num::ParseFloatError;
+impl<T:Scalar+FromStr> FromStr for XYZParticle<T> {
+    type Err = std::io::Error;
 
     fn from_str(line: &str) -> std::result::Result<Self, Self::Err> {
         let elems: std::vec::Vec<&str> = line.split_whitespace().collect();
+        if elems.len() != 4 {
+            return Err(Error::new(ErrorKind::InvalidInput, "invalid line"));
+        }
         let name = elems[0];
-        let x    = elems[1].parse::<f64>()?;
-        let y    = elems[2].parse::<f64>()?;
-        let z    = elems[3].parse::<f64>()?;
-        Ok(XYZParticle{ name: name.to_string(), coord: nalgebra::Vector3::new(x, y, z)})
+        let x    = elems[1].parse::<T>().map_err(|_| Error::new(ErrorKind::InvalidInput, "ParseNumError"))?;
+        let y    = elems[2].parse::<T>().map_err(|_| Error::new(ErrorKind::InvalidInput, "ParseNumError"))?;
+        let z    = elems[3].parse::<T>().map_err(|_| Error::new(ErrorKind::InvalidInput, "ParseNumError"))?;
+        Ok(XYZParticle::<T>{name: name.to_string(), coord: nalgebra::Vector3::new(x, y, z)})
     }
 }
 
-fn read_xyz_snapshot(filename: &str) -> std::io::Result<XYZParticleVec> {
+fn read_xyz_snapshot<T:Scalar+FromStr>(filename: &str) -> std::io::Result<std::vec::Vec<XYZParticle<T>>> {
     let mut fbuf = std::io::BufReader::new(std::fs::File::open(filename)?);
 
     let mut line = std::string::String::new();
@@ -47,10 +48,10 @@ fn read_xyz_snapshot(filename: &str) -> std::io::Result<XYZParticleVec> {
     println!("2nd line: {}", line);
     line.clear();
 
-    let mut snapshot = XYZParticleVec::with_capacity(*number_of_particles);
+    let mut snapshot = std::vec::Vec::with_capacity(*number_of_particles);
     for _ in 0..*number_of_particles {
         fbuf.read_line(&mut line)?;
-        snapshot.push(line.parse::<XYZParticle>().map_err(
+        snapshot.push(line.parse::<XYZParticle<T>>().map_err(
             |_| std::io::Error::new(
                 std::io::ErrorKind::InvalidInput,
                 "failed to read xyz particle"
@@ -61,7 +62,7 @@ fn read_xyz_snapshot(filename: &str) -> std::io::Result<XYZParticleVec> {
 }
 
 fn main() {
-    let xyz_snapshot = read_xyz_snapshot("example.xyz").expect("read xyz file");
+    let xyz_snapshot = read_xyz_snapshot::<f64>("example.xyz").expect("read xyz file");
 
     println!("found {} particles", xyz_snapshot.len());
     for particle in xyz_snapshot.iter() {
