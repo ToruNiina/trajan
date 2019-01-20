b9538663392e3843c2f912a6733ab062aef962fa
ToruNiina <niina.toru.68u@gmail.com>
2019-01-20 17:42:21 +0900

    split Format error and Parse*Error

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,56 @@
-#[derive(Debug, Fail)]
-pub enum ErrorKind {
-    #[fail(display = "IO error")]
-    Io,
-    #[fail(display = "Invalid Format")]
-    Format,
-}
-
-/* ----------- failure boilerplate ----------- */
-
 use std::fmt;
 use std::fmt::Display;
 use failure::{Backtrace, Context, Fail};
+
+#[derive(Debug, Fail)]
+pub enum ErrorKind {
+    #[fail(display = "I/O Error")]
+    Io,
+    #[fail(display = "Invalid Integer Format")]
+    ParseIntError,
+    #[fail(display = "Invalid Float Format")]
+    ParseFloatError,
+    #[fail(display = "Invalid Format: {:?}", error)]
+    Format{
+        error: std::string::String
+    },
+}
 
 #[derive(Debug)]
 pub struct Error {
     inner: Context<ErrorKind>,
 }
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+/* ----------- conversion between errors ----------- */
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Error {
+        Error {
+            inner: error.context(ErrorKind::Io),
+        }
+    }
+}
+
+impl From<std::num::ParseFloatError> for Error {
+    fn from(error: std::num::ParseFloatError) -> Error {
+        Error {
+            inner: error.context(ErrorKind::ParseFloatError),
+        }
+    }
+}
+
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Error {
+        Error {
+            inner: error.context(ErrorKind::ParseIntError),
+        }
+    }
+}
+
+/* ----------- failure boilerplate ----------- */
+
 
 impl Fail for Error {
     fn cause(&self) -> Option<&Fail> {
@@ -37,7 +72,6 @@ impl Error {
     pub fn new(inner: Context<ErrorKind>) -> Error {
         Error { inner }
     }
-
     pub fn kind(&self) -> &ErrorKind {
         self.inner.get_context()
     }
@@ -54,31 +88,5 @@ impl From<ErrorKind> for Error {
 impl From<Context<ErrorKind>> for Error {
     fn from(inner: Context<ErrorKind>) -> Error {
         Error { inner }
-    }
-}
-
-/* ----------- conversion between errors ----------- */
-
-impl From<std::io::Error> for Error {
-    fn from(error: std::io::Error) -> Error {
-        Error {
-            inner: error.context(ErrorKind::Io),
-        }
-    }
-}
-
-impl From<std::num::ParseFloatError> for Error {
-    fn from(error: std::num::ParseFloatError) -> Error {
-        Error {
-            inner: error.context(ErrorKind::Format),
-        }
-    }
-}
-
-impl From<std::num::ParseIntError> for Error {
-    fn from(error: std::num::ParseIntError) -> Error {
-        Error {
-            inner: error.context(ErrorKind::Format),
-        }
     }
 }
