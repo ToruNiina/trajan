6e0ccd5cbc8dd3996073d6305966dc5b17c1dd0f
ToruNiina <niina.toru.68u@gmail.com>
2019-01-22 01:32:42 +0900

    use xyzwriter in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
     let reader = trajan::xyz::XYZReader::open_pos("example.xyz").unwrap().f32();
+    let mut writer = trajan::xyz::XYZWriter::new(std::io::stdout());
 
     for snapshot in reader {
-        println!("{:?}", snapshot.which());
-        for particle in snapshot.particles {
-            println!("{}", particle);
-        }
+        writer.write_snapshot(snapshot).expect("write a snapshot into stdout");
     }
 }
